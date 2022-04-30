a894ecbf295b9dcb4a71b1e903296aee4352e97d
xiaolongMacM1 <zlxworkmail@163.com>
2022-04-30 11:08:57 +0800

    debug le yi xia

--- test2.go
+++ test2.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"test428/test1"
+	// "test428/test1"
 	// "github.com/zhaofirst/test1Project"
 	// "test1/test1"
 )
@@ -15,6 +15,6 @@ func main() {
 	fmt.Println("做了更改")
 	// test1Project.Test2()
 	// fmt.Println("又加了一行")
-	test1.Test1()
+	// test1.Test1()
 
 }
