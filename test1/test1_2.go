3c1d882ff85a606529c7c50b18d8232e6f8b865a
xiaolongMacM1 <zlxworkmail@163.com>
2022-04-30 09:47:10 +0800

    test1加了新的test1_2

---
+++ test1/test1_2.go
@@ -0,0 +1,14 @@
+package test1
+
+import (
+	"fmt"
+	// "github.com/zhaofirst/test1Project"
+)
+
+func Test1_2() {
+	// var a int = 10
+
+	// fmt.Println("又加了一行")
+	fmt.Println("only this test 1_2")
+
+}
